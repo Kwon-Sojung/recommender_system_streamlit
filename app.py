8c0796d1d880a98b9defc55ce418a44b93986f49
Kwon-Sojung <105343406+Kwon-Sojung@users.noreply.github.com>
2022-08-17 10:31:04 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,22 @@
+def app():
+    API_CONFIG = config.NaverOpenAPIConfig
+    tokenizer = load_tokenizer()
+    model = load_model()
+
+    st.title("웹툰 추천")
+    title = st.text_input("정확한 웹툰 제목을 입력하고 Enter를 눌러주세요. (카카오/네이버 웹툰만 입력 가능)")
+    select_area = st.empty()
+
+    st.write("""---""")
+
+    placeholder = st.empty()
+    webtoon_aria = st.empty()
+
+    st.write("""---""")
+    rating_area = st.empty()
+    tab_area = st.empty()
+
+    if not title:
+        return placeholder.success("입력을 기다리고 있어요... ")
+
+ 
