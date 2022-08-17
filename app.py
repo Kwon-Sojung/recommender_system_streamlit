f076cb6876a7b5d2257822f2722204bacd346bcc
Kwon-Sojung <105343406+Kwon-Sojung@users.noreply.github.com>
2022-08-17 11:13:07 +0900

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ st.write("""---""")
 rating_area = st.empty()
 tab_area = st.empty()
 
-# if not title:
-#     return placeholder.success("입력을 기다리고 있어요... ")
+if not title:
+        return placeholder.success("입력을 기다리고 있어요... ")
 
  
