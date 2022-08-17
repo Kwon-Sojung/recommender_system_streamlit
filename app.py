52e5aabd40b81aaa3d5ab33811fc85bb9440b76c
Kwon-Sojung <105343406+Kwon-Sojung@users.noreply.github.com>
2022-08-17 11:11:36 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,12 +7,12 @@ select_area = st.empty()
 
 st.write("""---""")
 
-# placeholder = st.empty()
-# webtoon_area = st.empty()
+placeholder = st.empty()
+webtoon_area = st.empty()
 
-# st.write("""---""")
-# rating_area = st.empty()
-# tab_area = st.empty()
+st.write("""---""")
+rating_area = st.empty()
+tab_area = st.empty()
 
 # if not title:
 #     return placeholder.success("입력을 기다리고 있어요... ")
