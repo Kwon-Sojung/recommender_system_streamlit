e90808f24382fd736fe9b6404a1a077ef2db36a5
Kwon-Sojung <105343406+Kwon-Sojung@users.noreply.github.com>
2022-08-17 22:54:13 +0900

    Update genre_model.py

--- genre_model.py
+++ genre_model.py
@@ -4,8 +4,8 @@ import numpy as np
 
 from sklearn.metrics.pairwise import cosine_similarity
 
-df_raw = pd.read_csv("webtoon_total_final.csv")
-df = df_raw[['title','score', 'genre']]
+df = pd.read_csv("webtoon_total_final.csv")
+df = df[['title','score', 'genre']]
 df.genre = df.genre.str.strip('['']')
 tmp = pd.get_dummies(df.genre)
 
@@ -21,7 +21,7 @@ def col_change(df):
 col_change(tmp)
 
 genre_df = col_change(pd.get_dummies(df.genre))
-df = pd.concat([df.title, df.score, genre_df , df_raw.image], axis=1)
+df = pd.concat([df.title, df.score, genre_df], axis=1)
 
 df.columns = df.columns.str.strip('\'')
 
