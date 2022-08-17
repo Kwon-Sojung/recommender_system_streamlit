e3927243a5ab437ef391f52071b872473ce8d43f
Kwon-Sojung <105343406+Kwon-Sojung@users.noreply.github.com>
2022-08-17 19:00:03 +0900

    Update app.py

--- app.py
+++ app.py
@@ -10,15 +10,22 @@ st.set_page_config(
 title_name = []
 st.markdown("# 소개글 📚")
 title = st.text_input("👇 정확한 웹툰 제목을 입력하고 Enter를 눌러주세요. (카카오/네이버 웹툰만 입력 가능)")
-st.empty()
-st.write("""—--""")
-st.empty()
-title_name.append(title)
+# st.empty()
+# st.write("""—--""")
+# st.empty()
+# title_name.append(title)
 
-if not title:
-    print(st.empty().info("입력 기다리는 중…⏳"))
-    image = Image.open('wating.jpg')
-    st.image(image)
+# if not title:
+#     print(st.empty().info("입력 기다리는 중…⏳"))
+#     image = Image.open('wating.jpg')
+#     st.image(image)
 
-else:
-   print(st.write(title_name))
+# else:
+#    print(st.write(title_name))
+
+options = st.multiselect(
+     'What are your favorite colors',
+     ['Green', 'Yellow', 'Red', 'Blue'],
+     ['Yellow', 'Red'])
+
+st.write('You selected:', options)
