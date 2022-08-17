1701583d07711351cfa93c82d880fff9c7387a2d
Kwon-Sojung <105343406+Kwon-Sojung@users.noreply.github.com>
2022-08-17 22:14:30 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from PIL import Image
 import pandas as pd
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
-from model import genre_model
+from genre_model import genre_model
 
 
 st.set_page_config(
