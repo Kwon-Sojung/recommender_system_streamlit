69e95aaac3d5f0c178a9815cf43c8886dba9e715
Kwon-Sojung <105343406+Kwon-Sojung@users.noreply.github.com>
2022-08-17 10:47:05 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,20 @@
 import streamlit as st
 
 
+st.title("웹툰 추천")
+title = st.text_input("정확한 웹툰 제목을 입력하고 Enter를 눌러주세요. (카카오/네이버 웹툰만 입력 가능)")
+select_area = st.empty()
 
-    st.title("웹툰 추천")
-    title = st.text_input("정확한 웹툰 제목을 입력하고 Enter를 눌러주세요. (카카오/네이버 웹툰만 입력 가능)")
-    select_area = st.empty()
+st.write("""---""")
 
-    st.write("""---""")
+placeholder = st.empty()
+webtoon_area = st.empty()
 
-    placeholder = st.empty()
-    webtoon_aria = st.empty()
+st.write("""---""")
+rating_area = st.empty()
+tab_area = st.empty()
 
-    st.write("""---""")
-    rating_area = st.empty()
-    tab_area = st.empty()
-
-    if not title:
-        return placeholder.success("입력을 기다리고 있어요... ")
+# if not title:
+#     return placeholder.success("입력을 기다리고 있어요... ")
 
  
