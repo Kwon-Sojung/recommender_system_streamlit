1721a814ca30c6a9306f724d6b433724b25ad40f
Kwon-Sojung <105343406+Kwon-Sojung@users.noreply.github.com>
2022-08-17 11:39:25 +0900

    Update app.py

--- app.py
+++ app.py
@@ -17,4 +17,12 @@ tab_area = st.empty()
 if not title:
     print(placeholder.success("입력을 기다리고 있어요... "))
 
+# Add a placeholder 진행 상황 바
+latest_iteration = st.empty()
+bar = st.progress(0)
+for i in range(100):
+    # Update the progress bar with each iteration.
+    latest_iteration.text(f'Loading {i+1}')
+    bar.progress(i + 1)
+    time.sleep(0.01)
  
