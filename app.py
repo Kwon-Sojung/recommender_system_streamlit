c809f9e675f2948036ce451c2d34c1c14091af40
Kwon-Sojung <105343406+Kwon-Sojung@users.noreply.github.com>
2022-08-17 11:11:15 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ import streamlit as st
 
 st.title("웹툰 추천")
 title = st.text_input("정확한 웹툰 제목을 입력하고 Enter를 눌러주세요. (카카오/네이버 웹툰만 입력 가능)")
-# select_area = st.empty()
+select_area = st.empty()
 
-# st.write("""---""")
+st.write("""---""")
 
 # placeholder = st.empty()
 # webtoon_area = st.empty()
