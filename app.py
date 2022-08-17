a391569e77e7d942daacf513c257b2c274a796b2
Kwon-Sojung <105343406+Kwon-Sojung@users.noreply.github.com>
2022-08-17 22:12:40 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 from model import genre_model
 
+
 st.set_page_config(
     page_title="Webtoon Recommender App Page Introduction",
     page_icon="📚",
