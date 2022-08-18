d82427b44a4ad16111181fa3b0db99af6cbc5583
Kwon-Sojung <105343406+Kwon-Sojung@users.noreply.github.com>
2022-08-18 14:28:27 +0900

    Update app.py

--- app.py
+++ app.py
@@ -53,8 +53,8 @@ if options:
     genre_recommend_df.rename(columns={"title":"제목", "image":"웹툰", "genre":"장르", "artist":"작가", "story":"줄거리", "score":"평점"},
                                        inplace=True)
 
-    table = genre_recommend_df.to_html(escape=False,index=False,
-                                         float_format='{0:.4g}'.format,formatters=dict(웹툰=to_img_tag))
+    table = HTML(genre_recommend_df.to_html(escape=False,index=False,
+                                         float_format='{0:.4g}'.format,formatters=dict(웹툰=to_img_tag)))
 #     df = pd.read_html(table)
 #     df = pd.DataFrame(df)
   
