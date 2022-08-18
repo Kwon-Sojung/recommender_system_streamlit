d2a62b94f1e9a9f6393de66ebebcc8cd6c294c6a
Kwon-Sojung <105343406+Kwon-Sojung@users.noreply.github.com>
2022-08-19 03:04:42 +0900

    Update image_model.py

--- image_model.py
+++ image_model.py
@@ -5,7 +5,7 @@ import numpy as np
 import random
 from math import dist
 
-df_euclidien_distance = pd.read_csv('Euclidean_distance_final.csv', index_col='Unnamed: 0')
+df_euclidien_distance = pd.read_csv('Eucliden_distance_final.csv')
     
 def single_distance(title):
     similar_df =df_euclidien_distance[[title]]
