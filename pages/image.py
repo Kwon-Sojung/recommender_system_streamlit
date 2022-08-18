85bd78850970ef2c33e4ce046c532a171801b36b
Kwon-Sojung <105343406+Kwon-Sojung@users.noreply.github.com>
2022-08-19 03:00:32 +0900

    Update image.py

--- pages/image.py
+++ pages/image.py
@@ -1,98 +1,76 @@
 import streamlit as st
-from st_aggrid import AgGrid, GridUpdateMode
-from st_aggrid.grid_options_builder import GridOptionsBuilder
-
-import seaborn as sns
-import matplotlib.pyplot as plt
-import koreanize_matplotlib
+from PIL import Image
 import pandas as pd
 import numpy as np
-import random
-from math import dist
-import pyarrow
-
 from sklearn.metrics.pairwise import cosine_similarity
-
+from image_model import image_recommendation
 from IPython.core.display import HTML
 
+
+
+st.set_page_config(
+    page_title="Webtoon Recommender App Page Introduction",
+    page_icon="📚",
+    layout="wide",
+)
+
 title_name = []
-st.markdown("# 그림체 기반 웹툰 추천 📚")
+st.markdown("# 장르 기반 웹툰 추천 📚")
 
 st.balloons()
 
-# 데이터 프레임 불러오고 전처리 하기
-df_origin = pd.read_csv("webtoon_total_final.csv")
+webtoon_df = pd.read_csv("webtoon_total_final.csv")
+title_list = webtoon_df["title"].tolist()
 
-raw_title_list = df_origin["title"].tolist()
-
-df = df_origin[['title','score', 'genre']]
-df.genre = df.genre.str.strip('['']')
-
-# input box 만들기
 options = st.multiselect(
      '👇 선호하는 웹툰 제목을 입력하고 Enter를 눌러주세요. (복수 입력 가능하며, 카카오/네이버 웹툰만 입력 가능)',
      title_list
      )
+
+
+# st.write('You selected:', options)
+
 select_area = st.empty()
 st.write("""---""")
 
-# if not options:
-#     print(st.empty().info("입력 기다리는 중…⏳"))
-#     image = Image.open('jamanchu_family.png')
-#     st.image(image)
-
-
-# 그림체 기반 추천 알고리즘
-df_euclidien_distance = pd.read_parquet('Euclidien_distance.parquet')
+if not options:
+    print(st.empty().info("입력 기다리는 중…⏳"))
+    image = Image.open('wating.jpg')
+    st.image(image)
+ 
+# if options:
+#     genre_recommend_df = genre_model(options)
+#     st.write(genre_recommend_df)
     
-def single_distance(title):
-    similar_df =df_euclidien_distance[[title]]
-    similar_df.columns = ['title']
-    return similar_df
-
-# 스타일 loss 값 저장한 csv에서 불러오는 부분
-title_list = df_euclidien_distance.index.tolist()
+   
+def to_img_tag(path):
+    return '<img src="'+ path + '" width="200" >'
 
 
-def image_recommendation(title_input):
-    if not title_input:
-        st.write("아직 선택한 웹툰이 없습니다")
-        return pd.DataFrame()
+if options:
+    genre_recommend_df = genre_model(options)
+    genre_recommend_df = genre_recommend_df[["title", "image", "genre", "artist", "story", "score"]]
+    genre_recommend_df.rename(columns={"title":"제목", "image":"웹툰", "genre":"장르", "artist":"작가", "story":"줄거리", "score":"평점"},
+                                       inplace=True)
+
+    table = HTML(genre_recommend_df.to_html(escape=False,index=False,
+                                         float_format='{0:.4g}'.format,formatters=dict(웹툰=to_img_tag)))
+#     df = pd.read_html(table)
+#     df = pd.DataFrame(df)
+  
+#     for l in range(10):
+#         l_title = genre_recommend_df["제목"].iloc(l)
+#         st.write(l_title)
+
+#     df=pd.DataFrame(html_table[1:], columns=html_table[0])
+    st.write(table)
+
+
+
     
-    # result 데이터 프레임 만들기
-    empty_list = [0 for i in range(len(df_euclidien_distance))]
-    result = pd.DataFrame()
-    result['result'] = empty_list
-    result.index = title_list
-    
-    for title in title_input:
-        tmp = single_distance(title)
-        result['result'] = result.values + tmp.values
-        
-    result.sort_values('result',inplace=True)
-    result.drop(title_input, inplace=True)
-    
-    result_title_list = result[:10].index.tolist()
-    
-    final_df = pd.DataFrame(columns=['title', 'artist', 
-                                     'genre', 'story',
-                                     'image', 'from', 'score'])
-    
-    for r in result_title_list:
-        tmp = df_origin[df_origin['title']== r]
-        final_df = pd.concat([final_df,tmp])
-
-    return final_df
 
 
-g = image_recommendation(title_input)
 
-if len(g) == 0:
-    st.write('웹툰 항목에서 최소 하나의 웹툰을 선택해주세요!')
+
     
-elif len(g) != 0:
-    df_result = g[['title', 'artist', 'genre','story','score','image']]
-    def to_img_tag(path):
-        return '<img src="'+ path + '" width="100" >'
-    a= HTML(df_result.to_html(escape=False,formatters=dict(image=to_img_tag)))
-    st.write(a)
+    
