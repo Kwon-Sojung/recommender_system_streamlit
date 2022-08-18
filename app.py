c367a8c48a9adfe5f1c4bf3eeebee266961c8df1
Kwon-Sojung <105343406+Kwon-Sojung@users.noreply.github.com>
2022-08-18 09:51:02 +0900

    Update app.py

--- app.py
+++ app.py
@@ -49,7 +49,6 @@ if options:
     genre_recommend_df = genre_recommend_df[["title", "image", "genre", "artist", "story"]]
 
     df = HTML(genre_recommend_df.to_html(escape=False,formatters=dict(image=to_img_tag)))
-#     st.write(df)
-    d_title = df["title"]
-    st.write(d_title)
+    st.write(df)
+    
     
