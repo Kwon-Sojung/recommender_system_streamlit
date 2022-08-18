4f48d05d3966e4b507ea817a750a0d3e59c0b9d5
Serin Lee <105341794+srinlin@users.noreply.github.com>
2022-08-19 03:46:30 +0900

    what is this

--- image_model.py
+++ image_model.py
@@ -9,7 +9,7 @@ df_euclidien_distance = pd.read_parquet('Euclidien_distance.parquet')
 df_euclidien_distance.genre = df_euclidien_distance.genre.str.strip('['']').str.replace("'","")
     
 def single_distance(title):
-    similar_df =df_euclidien_distance[[title]]
+    similar_df =df_euclidien_distance[[title]] #이게 뭐지?
     similar_df.columns = ['title']
     return similar_df
 
