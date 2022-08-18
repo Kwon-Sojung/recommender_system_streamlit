3e75a32a669cbee45348d24c6cc5d72a1f9ca0a4
Kwon-Sojung <105343406+Kwon-Sojung@users.noreply.github.com>
2022-08-19 03:10:18 +0900

    Update image_model.py

--- image_model.py
+++ image_model.py
@@ -5,7 +5,7 @@ import numpy as np
 import random
 from math import dist
 
-df_euclidien_distance = pd.read_parquet('Eucliden_distance_final.parquet')
+df_euclidien_distance = pd.read_parquet('Euclidien_distance_final.parquet')
     
 def single_distance(title):
     similar_df =df_euclidien_distance[[title]]
