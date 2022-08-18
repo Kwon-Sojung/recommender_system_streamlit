ffcce3f8461c98f63388494e4036c8a9f0c3ee4d
Kwon-Sojung <105343406+Kwon-Sojung@users.noreply.github.com>
2022-08-18 13:50:56 +0900

    Update app.py

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ if options:
 
     table = HTML(genre_recommend_df.to_html(escape=False,index=False,
                                          float_format='{0:.4g}'.format,formatters=dict(웹툰=to_img_tag)))
-    df = pd.read_html(table)
+    df = pd.read_html(table, "html.parser")
 #     for l in range(10):
 #         l_title = genre_recommend_df["제목"].iloc(l)
 #         st.write(l_title)
