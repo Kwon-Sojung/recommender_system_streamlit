be516194aba267f226cf5dcd240c7129de7ebe1e
Kwon-Sojung <105343406+Kwon-Sojung@users.noreply.github.com>
2022-08-19 03:42:06 +0900

    Update image.py

--- pages/image.py
+++ pages/image.py
@@ -7,7 +7,7 @@ from math import dist
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.metrics.pairwise import cosine_similarity
-from image_model import single_distance, image_recommendation
+# from image_model import single_distance, image_recommendation
 from IPython.core.display import HTML
 
 
@@ -42,8 +42,50 @@ if not options:
     image = Image.open('jamanchu_family.jpg')
     st.image(image)
  
+###
+df_euclidien_distance = pd.read_parquet('Euclidien_distance.parquet')
     
-   
+def single_distance(title):
+    similar_df =df_euclidien_distance[[title]]
+    similar_df.columns = ['title']
+    return similar_df
+
+# 스타일 loss 값 저장한 csv에서 불러오는 부분
+title_list = df_euclidien_distance.index.tolist()
+
+
+def image_recommendation(title_input):
+    if not title_input:
+        st.write("아직 선택한 웹툰이 없습니다")
+        return pd.DataFrame()
+    
+    # result 데이터 프레임 만들기
+    empty_list = [0 for i in range(len(df_euclidien_distance))]
+    result = pd.DataFrame()
+    result['result'] = empty_list
+    result.index = title_list
+    
+    for title in title_input:
+        tmp = single_distance(title)
+        result['result'] = result.values + tmp.values
+        
+    result.sort_values('result',inplace=True)
+    result.drop(title_input, inplace=True)
+    
+    result_title_list = result[:10].index.tolist()
+    
+    final_df = pd.DataFrame(columns=['title', 'artist', 
+                                     'genre', 'story',
+                                     'image', 'from', 'score'])
+    
+    for r in result_title_list:
+        tmp = df_origin[df_origin['title']== r]
+        final_df = pd.concat([final_df,tmp])
+
+    return final_df
+
+
+###   
 def to_img_tag(path):
     return '<img src="'+ path + '" width="200" >'
 
